79801431c1f5d0c137d4e165e8e64d77487f29e6
Fantom-ctrl <mara.sirazieva@yandex.ru>
2026-02-10 15:58:35 +0700

    Update

--- task0/doubleAndFloat.cpp
+++ task0/doubleAndFloat.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <iostream>
 
-using namespace std; 
 
 #ifndef USE_DOUBLE
 #define USE_DOUBLE 0
@@ -19,14 +18,14 @@ int main()
 {
     constexpr size_t N = 10'000'000;
     constexpr pres PI = static_cast<pres>(3.14159265358979323846);
-    vector<pres> data(N);
+    std::vector<pres> data(N);
 
     // Шаг аргумента синуса
     pres step = static_cast<pres>(2) * PI / static_cast<pres>(N);
 
     for (size_t i = 0; i < N; ++i) 
     {
-        data[i] = sin(step * static_cast<pres>(i));
+        data[i] = std::sin(step * static_cast<pres>(i));
     }
 
     pres sum = 0;
@@ -37,12 +36,12 @@ int main()
     }
 
     #if USE_DOUBLE
-    cout << "Array type: double\n";
+        std::cout << "Array type: double\n";
     #else
-        cout << "Array type: float\n";
+        std::cout << "Array type: float\n";
     #endif
 
-    cout << "Sum: " << sum << endl;
+    std::cout << "Sum: " << sum << std::endl;
 
     return 0;
 }
